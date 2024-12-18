8ad6b566f49368dcb78f2a69580914e29b1b6bdf
vidyasagarbhargava <vsagar19@gmail.com>
2024-12-18 21:55:51 +0530

    multiple changes

--- 01_Prerequiste/03_pytorch_basics/mnist.py
+++ 01_Prerequiste/03_pytorch_basics/mnist.py
@@ -13,6 +13,7 @@ batch_size = 32
 #1) Dataset creation
 train_dataset = torchvision.datasets.MNIST(root='./data', train=True, transform=transforms.ToTensor(), download=True)
 test_dataset = torchvision.datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor())
+
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
 
